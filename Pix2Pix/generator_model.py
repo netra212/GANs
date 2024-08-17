9281451c20cfa3448d3c6a6ff63ab9cd875882ce
netra2127 <netra200021kcbdr@gmail.com>
2024-08-17 20:21:56 +0100

    Generator Model

---
+++ Pix2Pix/generator_model.py
@@ -0,0 +1,27 @@
+import torch 
+import torch.nn as nn 
+
+class Block(nn.Module):
+    def __init__(self, in_channels, out_channels, down=True, act="relu", use_dropout=False):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 4, 2, 1, bias=False, padding_mode="reflect")
+            if down 
+            else nn.ConvTranspose2d(in_channels, out_channels, 4, 2, 1,bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU() if act=="relu" else nn.LeakyReLU(0.2),
+        )
+        self.use_dropout = use_dropout
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return self.dropout(x) if self.use_dropout else x
+
+class Generator(nn.Module):
+    def __init__(self, in_channels=3, features=64):
+        super().__init__()
+        
+        
+
+
